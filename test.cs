c6753e84ed6dcf76aa0108209851700e2394b638
손정우 <50861700+wonder21c@users.noreply.github.com>
2025-06-04 19:45:24 +0900

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,33 +1,21 @@
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System;
 
 namespace Calculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         string input = "";
-        double num = 0;
         double result = 0;
-        bool plusButton = false;
-        bool minusButton = false;
-        bool mulButton = false;
-        bool divButton = false;
+        string currentOperator = "";
+        bool isFirstInput = true;
 
         public MainWindow()
         {
             InitializeComponent();
         }
+
         private void button_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
@@ -37,161 +25,73 @@ namespace Calculator
             {
                 input += content;
                 textbox.Text = input;
-      
             }
-            
-            else if (content == "+")
+            else if (content == "+" || content == "-" || content == "*" || content == "/")
             {
-
-                if (double.TryParse(input, out num))
-                {
-                    result += num;
-                    input = "";
-                    if (minusButton || mulButton || divButton)
-                    {
-                        minusButton = false;
-                        mulButton = false;
-                        divButton = false;
-                    }
-                    plusButton = true;
-                }
+                ApplyPreviousOperation();
+                currentOperator = content;
             }
-
-            else if (content == "-")
-            {
-                if (double.TryParse(input, out num))
-                {
-                    if (result == 0)
-                    {
-                        result = num - result;
-                        input = "";
-                        if (plusButton || mulButton || divButton)
-                        {
-                            plusButton = false;
-                            mulButton = false;
-                            divButton = false;
-                        }
-                        minusButton = true;
-                    }
-                    else
-                        result -= num;
-                    input = "";
-                    if (plusButton || mulButton || divButton)
-                    {
-                        plusButton = false;
-                        mulButton = false;
-                        divButton = false;
-                    }
-                    minusButton = true;
-                }
-            }
-
-            else if (content == "*")
-            {
-                if (double.TryParse(input, out num))
-                {
-                    if (result <= 0)
-                    {
-                        result = 1;
-                    }
-
-                    result *= num;
-                    input = "";
-                    if (plusButton || minusButton || divButton)
-                    {
-                        plusButton = false;
-                        minusButton = false;
-                        divButton = false;
-                    }
-                    mulButton = true;
-                }
-            }
-
-            else if (content == "/")
-            {
-                if (double.TryParse(input, out num))
-                {
-                    if (result == 0)
-                    {
-                        result = num / 1;
-                        input = "";
-                        if (plusButton || mulButton || minusButton)
-                        {
-                            plusButton = false;
-                            mulButton = false;
-                            minusButton = false;
-                        }
-                        divButton = true;
-                    }
-                    else
-                    {
-                        result /= num;
-                        input = "";
-                        if (plusButton || mulButton || minusButton)
-                        {
-                            plusButton = false;
-                            mulButton = false;
-                            minusButton = false;
-                        }
-                        divButton = true;
-                    }
-                }
-            }
-
-
             else if (content == "=")
             {
-                if (plusButton && double.TryParse(input, out num))
-                {
-
-                    double finalResult = result + num;
-                    textbox.Text = finalResult.ToString();
-                    //input = finalResult.ToString();
-                    plusButton = false;
-                }
-
-                else if (minusButton && double.TryParse(input, out num))
-                {
-                    double finalResult = result - num;
-                    textbox.Text = finalResult.ToString();
-                    // input = finalResult.ToString();
-                    minusButton = false;
-                }
-
-                else if (mulButton && double.TryParse(input, out num))
-                {
-                    double finalResult = result * num;
-                    textbox.Text = finalResult.ToString();
-                    //input = finalResult.ToString();
-                    mulButton = false;
-                }
-
-                else if (divButton && double.TryParse(input, out num))
-                {
-                    double finalResult = result / num;
-                    textbox.Text = finalResult.ToString();
-                    //input = finalResult.ToString();
-                    divButton = false;
-
-                }
+                ApplyPreviousOperation();
+                textbox.Text = result.ToString();
+                input = result.ToString(); // 결과를 다음 입력으로 사용
+                currentOperator = "";
+                isFirstInput = true; // 다음 계산을 위해 초기화
             }
-
             else if (content == "C")
             {
                 input = "";
                 result = 0;
-                plusButton = false;
-                minusButton = false;
-                mulButton = false;
-                divButton = false;
+                currentOperator = "";
+                isFirstInput = true;
                 textbox.Text = "0";
             }
-
+            else if (content == "←")
+            {
+                if (!string.IsNullOrEmpty(input))
+                {
+                input = input.Substring(0, input.Length - 1);
+                textbox.Text = string.IsNullOrEmpty(input) ? "0" : input;
+                }
+            }
 
         }
 
-
+        private void ApplyPreviousOperation()
+        {
+            if (!string.IsNullOrEmpty(input))
+            {
+                double number;
+                if (double.TryParse(input, out number))
+                {
+                    if (isFirstInput)
+                    {
+                        result = number;
+                        isFirstInput = false;
+                    }
+                    else
+                    {
+                        switch (currentOperator)
+                        {
+                            case "+":
+                                result += number;
+                                break;
+                            case "-":
+                                result -= number;
+                                break;
+                            case "*":
+                                result *= number;
+                                break;
+                            case "/":
+                                result /= number;
+                                break;
+                        }
+                    }
+                }
+                input = "";
+                textbox.Text = result.ToString();
+            }
+        }
     }
-
-
 }
