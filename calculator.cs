5e07d942c9e84541ffa22aae0ea730ab86a48851
손정우 <50861700+wonder21c@users.noreply.github.com>
2025-06-04 19:26:40 +0900

    Update calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -17,55 +17,180 @@ namespace Calculator
     public partial class MainWindow : Window
     {
         string input = "";
-        int num1 = 0;
+        double num = 0;
+        double result = 0;
         bool plusButton = false;
+        bool minusButton = false;
+        bool mulButton = false;
+        bool divButton = false;
+
         public MainWindow()
         {
             InitializeComponent();
         }
-    private void button_Click(object sender, RoutedEventArgs e)
+        private void button_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
             string content = btn.Content.ToString();
 
-            
-            if (char.IsDigit(content, 0))
+            if (char.IsDigit(content, 0) || content == ".")
             {
                 input += content;
                 textbox.Text = input;
+      
             }
-
+            
             else if (content == "+")
             {
-                if (int.TryParse(input, out num1))
+
+                if (double.TryParse(input, out num))
                 {
+                    result += num;
                     input = "";
+                    if (minusButton || mulButton || divButton)
+                    {
+                        minusButton = false;
+                        mulButton = false;
+                        divButton = false;
+                    }
                     plusButton = true;
                 }
             }
 
+            else if (content == "-")
+            {
+                if (double.TryParse(input, out num))
+                {
+                    if (result == 0)
+                    {
+                        result = num - result;
+                        input = "";
+                        if (plusButton || mulButton || divButton)
+                        {
+                            plusButton = false;
+                            mulButton = false;
+                            divButton = false;
+                        }
+                        minusButton = true;
+                    }
+                    else
+                        result -= num;
+                    input = "";
+                    if (plusButton || mulButton || divButton)
+                    {
+                        plusButton = false;
+                        mulButton = false;
+                        divButton = false;
+                    }
+                    minusButton = true;
+                }
+            }
+
+            else if (content == "*")
+            {
+                if (double.TryParse(input, out num))
+                {
+                    if (result <= 0)
+                    {
+                        result = 1;
+                    }
+
+                    result *= num;
+                    input = "";
+                    if (plusButton || minusButton || divButton)
+                    {
+                        plusButton = false;
+                        minusButton = false;
+                        divButton = false;
+                    }
+                    mulButton = true;
+                }
+            }
+
+            else if (content == "/")
+            {
+                if (double.TryParse(input, out num))
+                {
+                    if (result == 0)
+                    {
+                        result = num / 1;
+                        input = "";
+                        if (plusButton || mulButton || minusButton)
+                        {
+                            plusButton = false;
+                            mulButton = false;
+                            minusButton = false;
+                        }
+                        divButton = true;
+                    }
+                    else
+                    {
+                        result /= num;
+                        input = "";
+                        if (plusButton || mulButton || minusButton)
+                        {
+                            plusButton = false;
+                            mulButton = false;
+                            minusButton = false;
+                        }
+                        divButton = true;
+                    }
+                }
+            }
+
+
             else if (content == "=")
             {
-                if (plusButton && int.TryParse(input, out int num2))
+                if (plusButton && double.TryParse(input, out num))
                 {
-                    int result = num1 + num2;
-                    textbox.Text = result.ToString();
-                    input = result.ToString();
+
+                    double finalResult = result + num;
+                    textbox.Text = finalResult.ToString();
+                    //input = finalResult.ToString();
                     plusButton = false;
+                }
+
+                else if (minusButton && double.TryParse(input, out num))
+                {
+                    double finalResult = result - num;
+                    textbox.Text = finalResult.ToString();
+                    // input = finalResult.ToString();
+                    minusButton = false;
+                }
+
+                else if (mulButton && double.TryParse(input, out num))
+                {
+                    double finalResult = result * num;
+                    textbox.Text = finalResult.ToString();
+                    //input = finalResult.ToString();
+                    mulButton = false;
+                }
+
+                else if (divButton && double.TryParse(input, out num))
+                {
+                    double finalResult = result / num;
+                    textbox.Text = finalResult.ToString();
+                    //input = finalResult.ToString();
+                    divButton = false;
+
                 }
             }
 
             else if (content == "C")
             {
                 input = "";
-                num1 = 0;
+                result = 0;
                 plusButton = false;
+                minusButton = false;
+                mulButton = false;
+                divButton = false;
                 textbox.Text = "0";
             }
-            ;
+
+
         }
 
-       
+
     }
 
 
