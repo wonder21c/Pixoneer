97e2afb7227b1b31a6cfff47fbf584ae13935785
손정우 <50861700+wonder21c@users.noreply.github.com>
2025-06-05 11:36:44 +0900

    Update test.cs

--- test.cs
+++ test.cs
@@ -11,20 +11,19 @@ using System.Windows.Shapes;
 
 namespace Calculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         string input = "";
         double num = 0;
         double result = 0;
         string currentContent = "";
+        string back = " ";
         bool firstNum = true;
         public MainWindow()
         {
             InitializeComponent();
         }
+
         private void button_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
@@ -66,13 +65,79 @@ namespace Calculator
                 textbox.Text = "0";
             }
 
-
-
-
+            else if (content == "Backspace") 
+            {
+                if (!string.IsNullOrEmpty(input))
+                {
+                    input = input.Substring(0, input.Length - 1);
+                    textbox.Text = string.IsNullOrEmpty(input) ? "0" : input;
+                }
+            
+            }
 
         }
 
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9)
+            {
+                string num = (e.Key - Key.D0).ToString();
+                buttonPressed(num);
+            }
 
+            else if(e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+                {
+                string num = (e.Key - Key.NumPad0).ToString();
+                buttonPressed(num);
+            }
+
+            else if(e.Key == Key.Add || e.Key == Key.OemPlus && (Keyboard.Modifiers & ModifierKeys.Shift ) != 0)
+            {
+                buttonPressed("+");
+            }
+
+            else if(e.Key == Key.OemMinus || e.Key == Key.Subtract)
+            {
+                buttonPressed("-");
+            }
+            else if(e.Key == Key.Multiply)
+            {
+                buttonPressed("*");
+            }
+            else if(e.Key == Key.Divide || e.Key == Key.Oem2)
+            {
+                buttonPressed("/");
+            }
+
+            else if (e.Key == Key.Enter)
+            {
+                buttonPressed("=");
+            }
+
+            else if (e.Key == Key.Back)
+            {
+                buttonPressed("Backspace");
+            }
+
+            else if (e.Key == Key.Escape)
+            {
+                buttonPressed("C");
+            }
+
+            else if (e.Key == Key.Decimal || e.Key == Key.OemPeriod)
+            {
+                buttonPressed(".");
+            }
+        }
+        
+        private void buttonPressed(string content)
+        {
+            Button button1 = new Button();
+            button1.Content = content;
+            button_Click(button1, null);
+
+        }
+        
 
         private void cal()
         {
@@ -111,5 +176,10 @@ namespace Calculator
             }
         }
 
+        private void Window_KeyDown_1(object sender, KeyEventArgs e)
+        {
+
+        }
     } 
+
 }    
