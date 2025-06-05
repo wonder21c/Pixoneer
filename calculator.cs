c3ecb90f795f3004e0b89e3727f84a2150a9d8c2
손정우 <50861700+wonder21c@users.noreply.github.com>
2025-06-05 16:18:28 +0900

    Update calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -11,187 +11,164 @@ using System.Windows.Shapes;
 
 namespace Calculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         string input = "";
         double num = 0;
         double result = 0;
-        bool plusButton = false;
-        bool minusButton = false;
-        bool mulButton = false;
-        bool divButton = false;
-
+        string currentContent = "";
+        bool firstNum = true;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void CalcFunc(string func)
+        {
+            if (char.IsDigit(func, 0) || func == ".")
+            {
+                input += func;
+                textbox.Text = input;
+            }
+            else if (func == "+" || func == "-" || func == "*" || func == "/")
+            {
+                Calc();
+                currentContent = func;
+            }
+            else if (func == "=")
+            {
+                Calc();
+                textbox.Text = result.ToString();
+                input = result.ToString();
+                currentContent = "";
+                firstNum = true;
+
+            }
+            else if (func == "C")
+            {
+                input = "";
+                result = 0;
+                currentContent = "";
+                firstNum = true;
+                textbox.Text = "0";
+            }
+            else if (func == "Backspace")
+            {
+                if (!string.IsNullOrEmpty(input))
+                {
+                    input = input.Substring(0, input.Length - 1);
+                    textbox.Text = string.IsNullOrEmpty(input) ? "0" : input;
+                }
+
+            }
+        }
+
         private void button_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
             string content = btn.Content.ToString();
 
-            if (char.IsDigit(content, 0) || content == ".")
+            if(content != null && content != string.Empty)
             {
-                input += content;
-                textbox.Text = input;
-      
+                CalcFunc(content);
             }
-            
-            else if (content == "+")
-            {
-
-                if (double.TryParse(input, out num))
-                {
-                    result += num;
-                    input = "";
-                    if (minusButton || mulButton || divButton)
-                    {
-                        minusButton = false;
-                        mulButton = false;
-                        divButton = false;
-                    }
-                    plusButton = true;
-                }
-            }
-
-            else if (content == "-")
-            {
-                if (double.TryParse(input, out num))
-                {
-                    if (result == 0)
-                    {
-                        result = num - result;
-                        input = "";
-                        if (plusButton || mulButton || divButton)
-                        {
-                            plusButton = false;
-                            mulButton = false;
-                            divButton = false;
-                        }
-                        minusButton = true;
-                    }
-                    else
-                        result -= num;
-                    input = "";
-                    if (plusButton || mulButton || divButton)
-                    {
-                        plusButton = false;
-                        mulButton = false;
-                        divButton = false;
-                    }
-                    minusButton = true;
-                }
-            }
-
-            else if (content == "*")
-            {
-                if (double.TryParse(input, out num))
-                {
-                    if (result <= 0)
-                    {
-                        result = 1;
-                    }
-
-                    result *= num;
-                    input = "";
-                    if (plusButton || minusButton || divButton)
-                    {
-                        plusButton = false;
-                        minusButton = false;
-                        divButton = false;
-                    }
-                    mulButton = true;
-                }
-            }
-
-            else if (content == "/")
-            {
-                if (double.TryParse(input, out num))
-                {
-                    if (result == 0)
-                    {
-                        result = num / 1;
-                        input = "";
-                        if (plusButton || mulButton || minusButton)
-                        {
-                            plusButton = false;
-                            mulButton = false;
-                            minusButton = false;
-                        }
-                        divButton = true;
-                    }
-                    else
-                    {
-                        result /= num;
-                        input = "";
-                        if (plusButton || mulButton || minusButton)
-                        {
-                            plusButton = false;
-                            mulButton = false;
-                            minusButton = false;
-                        }
-                        divButton = true;
-                    }
-                }
-            }
-
-
-            else if (content == "=")
-            {
-                if (plusButton && double.TryParse(input, out num))
-                {
-
-                    double finalResult = result + num;
-                    textbox.Text = finalResult.ToString();
-                    //input = finalResult.ToString();
-                    plusButton = false;
-                }
-
-                else if (minusButton && double.TryParse(input, out num))
-                {
-                    double finalResult = result - num;
-                    textbox.Text = finalResult.ToString();
-                    // input = finalResult.ToString();
-                    minusButton = false;
-                }
-
-                else if (mulButton && double.TryParse(input, out num))
-                {
-                    double finalResult = result * num;
-                    textbox.Text = finalResult.ToString();
-                    //input = finalResult.ToString();
-                    mulButton = false;
-                }
-
-                else if (divButton && double.TryParse(input, out num))
-                {
-                    double finalResult = result / num;
-                    textbox.Text = finalResult.ToString();
-                    //input = finalResult.ToString();
-                    divButton = false;
-
-                }
-            }
-
-            else if (content == "C")
-            {
-                input = "";
-                result = 0;
-                plusButton = false;
-                minusButton = false;
-                mulButton = false;
-                divButton = false;
-                textbox.Text = "0";
-            }
-
-
         }
 
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
 
-    }
+            
+
+            if (e.Key >= Key.D0 && e.Key <= Key.D9)
+            {
+                string num = (e.Key - Key.D0).ToString();
+                CalcFunc(num);
+            }
+
+            else if(e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+                {
+                string num = (e.Key - Key.NumPad0).ToString();
+                CalcFunc(num);
+            }
+
+            else if(e.Key == Key.Add || e.Key == Key.OemPlus && (Keyboard.Modifiers & ModifierKeys.Shift ) != 0)
+            {
+                CalcFunc("+");
+            }
+
+            else if(e.Key == Key.OemMinus || e.Key == Key.Subtract)
+            {
+                CalcFunc("-");
+            }
+            else if(e.Key == Key.Multiply)
+            {
+                CalcFunc("*");
+            }
+            else if(e.Key == Key.Divide || e.Key == Key.Oem2)
+            {
+                CalcFunc("/");
+            }
+
+            else if (e.Key == Key.Enter)
+            {
+                CalcFunc("=");
+            }
+
+            else if (e.Key == Key.Back)
+            {
+                CalcFunc("Backspace");
+            }
+
+            else if (e.Key == Key.Escape)
+            {
+                CalcFunc("C");
+            }
+
+            else if (e.Key == Key.Decimal || e.Key == Key.OemPeriod)
+            {
+                CalcFunc(".");
+            }
+        }
+        
+
+        private void Calc()
+        {
+            if (!string.IsNullOrEmpty(input))
+            {
+
+                if (double.TryParse(input, out num))
+                {
+                    if (firstNum)
+                    {
+                        result = num;
+                        firstNum = false;
+                    }
+                    else
+                    {
+                        switch (currentContent)
+                        {
+                            case "+":
+                                result += num; break;
+
+                            case "-":
+                                result -= num; break;
+
+                            case "*":
+                                result *= num; break;
+
+                            case "/":
+                                result /= num; break;
 
 
-}
+                        }
+                    }
+                }
+                input = "";
+                textbox.Text = result.ToString();
+            }
+        }
+
+       
+    } 
+
+}    
