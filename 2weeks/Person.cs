0606fc3637fe79e378f4ad4e90409587b777f956
손정우 <50861700+wonder21c@users.noreply.github.com>
2025-06-08 17:13:34 +0900

    Update and rename d to Person.cs

---
+++ 2weeks/Person.cs
@@ -0,0 +1,32 @@
+
+namespace AddressBook.Model
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string team { get; set; }
+        public string grade { get; set; }
+        public string phoneNum { get; set; }
+        public string email { get; set; }
+
+        public override string ToString()
+        {
+            return $"{name}|{team}|{grade}|{phoneNum}|{email}";
+        }
+
+        public static Person FromString(string line)
+        {
+            var parts = line.Split('|');
+            if (parts.Length != 5) return null;
+
+            return new Person
+            {
+                name = parts[0],
+                team = parts[1],
+                grade = parts[2],
+                phoneNum = parts[3],
+                email = parts[4]
+            };
+        }
+    }
+}
